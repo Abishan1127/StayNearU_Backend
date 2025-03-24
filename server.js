a269e011bb84677e97941a61b54660899f2bde95
Abishan1127 <kunananthan19abishan@gmail.com>
2025-03-24 23:26:47 +0530

    updates

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
   credentials: true, 
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
 }));
-
+console.log(process.env.FE_URL, process.env.ADMIN_URL);
 app.use(express.json()); 
 app.use(cookieParser()); 
 app.use(bodyParser.json());
